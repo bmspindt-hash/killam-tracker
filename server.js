5e6af1e10038692125de128a4984683dbf0f21f6
bmspindt-hash <bmspindt@gmail.com>
2026-07-10 16:33:41 -0500

    Change server port from 3000 to 5432

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path     = require("path");
 const { Pool } = require("pg");
 
 const app  = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5432;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
